a19cbfdab49f5bf126b7368d3fd4cc759cef6cb6
Javier A Chaves Camargo <javi@ou.edu>
2020-08-28 21:46:54 -0500

    1st time

---
+++ R/myf.R
@@ -0,0 +1,13 @@
+#' Square Function
+#' Takes vector and returns a vector of squared components
+#'
+#' @param x
+#'
+#' @return a vector of squared components
+#' @export
+#'
+#' @examples
+#' x=1:30;myf(x)
+myf=function(x){
+  x^2
+}
